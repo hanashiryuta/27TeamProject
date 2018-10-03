176e18d343396b9bfee58efb3f72decf9a361693
TokisakiUrusi <38412514+TokisakiUrusi@users.noreply.github.com>
2018-10-03 16:14:56 +0900

    エネミーの攻撃と攻撃用の弾丸が画面外に行ったら消滅する処理
    AttackEnemy.cs
    一定時間で弾丸を生成する
    Bullet.cs
    常にスピード分進み続け、画面外に行ったら消滅する

---
+++ 27TeamProject/Assets/AttackEnemy.cs
@@ -0,0 +1,35 @@
+﻿//
+//10月3日
+//作成者：安部崇寛
+//エネミーの攻撃
+//
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AttackEnemy : MonoBehaviour {
+
+    [SerializeField]
+    GameObject Bullet;//生成する弾丸を参照
+    [SerializeField]
+    float time;//発射する間隔
+
+    float bulletTime;
+	// Use this for initialization
+	void Start () {
+        bulletTime = time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //一定時間ごとに弾丸を生成
+        bulletTime -= Time.deltaTime;
+        if (bulletTime < 0)
+        {
+            Instantiate(Bullet,transform.position,Quaternion.identity);
+            bulletTime = time;
+        }
+	}
+}
