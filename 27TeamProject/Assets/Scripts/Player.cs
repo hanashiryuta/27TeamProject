fa5a2c54fd418c9adeb41414f1cc60a752bbdac9
KPC-017A1383\K017A1383 <yutanar10@icloud.com>
2018-10-05 14:36:56 +0900

    StageSelectScene作成
    タグの追加（WarpGate1）
    フォルダの追加
    
    Prefabs
    Nametransprot
    Player
    SelectManager
    Stage1
    Stage1-1
    Stage1-2
    Stage1-3
    Stage1-4
    Stage1-5
    stageback0
    stageback1
    
    Scenes
    Select
    
    Scripts
    Name.cs
    PlayerMove.cs
    SelectManager.cs
    WarpGate.cs
    
    Textures
    title.png

---
+++ 27TeamProject/Assets/Scripts/Player.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState
+{
+    NORMALMOVE,
+    HOOKMOVE,
+}
+
+public class Player : MonoBehaviour
+{
+
+    Rigidbody2D rigid;
+    public float moveSpeed;
+    public float moveForceMultiplier = 50;
+    public float jumpPower;
+    bool isJumpFlag;
+    PlayerState playerState = PlayerState.NORMALMOVE;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        switch (playerState)
+        {
+            case PlayerState.NORMALMOVE:
+                Move();
+                HookPoint();
+                if (Input.GetButtonDown("Jump"))
+                    Jump();
+                break;
+            case PlayerState.HOOKMOVE:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// 移動処理
+    /// </summary>
+    public void Move()
+    {
+        Vector2 moveVector = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, 0);
+        Vector2 rigidVelocity = new Vector2(rigid.velocity.x, 0);
+        rigid.AddForce(moveForceMultiplier * (moveVector - rigidVelocity));
+    }
+
+    public void Jump()
+    {
+        if (isJumpFlag)
+        {
+            isJumpFlag = false;
+            rigid.AddForce(Vector2.up * jumpPower);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumpFlag = true;
+        }
+    }
+
+    void HookPoint()
+    {
+
+    }
+
+}
---
+++ 27TeamProject/Assets/Scripts/WrapGate.cs
@@ -0,0 +1,36 @@
+﻿//
+//10月5日
+//田中悠斗
+//ゲートに当たったか判定するクラス
+//
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WrapGate : MonoBehaviour {
+
+    public string stageName;
+    public SelectManager selectManager;
+
+    
+   
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            GameObject.Find("Nametransprot");
+            selectManager.Change();
+            Debug.Log(stageName);
+        }
+    }
+}
