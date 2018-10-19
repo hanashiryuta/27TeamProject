3be1e2c43fcb39b800d7910cfcaf5710cbace162
hanashiryuta <kbrn.kabirunrun@gmail.com>
2018-10-19 10:59:05 +0900

    Revert "フォルダの追加、ブロック追加"
    フォルダの追加をなかったことに

---
+++ 27TeamProject/Assets/MoveEnemy.cs
@@ -0,0 +1,19 @@
+﻿//
+//10月3日
+//作成者：安部崇寛
+//移動だけするエネミークラス
+//
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnemy : Enemy {
+    
+	// Update is called once per frame
+	public override void Update () {
+        base.Update();
+        Move();
+
+	}
+}
