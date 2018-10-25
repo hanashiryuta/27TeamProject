587a2ab3895369bc38b3650aee9764afdd4e6985
hanashiryuta <kbrn.kabirunrun@gmail.com>
2018-10-25 19:15:41 +0900

    プレイヤー挙動調整、仮アニメ追加、フック調整、カメラ調整
    Hook.cs
    ノックバック攻撃追加
    
    Player.cs
    たたきつけ追加、移動調整
    
    TestFloor.cs:新規作成
    床確認用スクリプト
    
    Texture
    Player_Run画像追加
    Player_Shot画像追加
    Test_Run画像追加
    Arm画像追加
    
    Animation:新規作成
    Player_Runアニメ追加
    Player_Shotアニメ追加
    Test_Runアニメ追加
    
    Prefab
    カメラ角度調整
    それに伴う軽微な調整

---
+++ 27TeamProject/Assets/TestFloors.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestFloors : MonoBehaviour
+{
+
+    public float blockSize;
+    public GameObject block;
+    public float XBlockNum;
+    public float ZBlockNum;
+
+    // Use this for initialization
+    void Start()
+    {
+        block.transform.localScale = new Vector3(blockSize, blockSize, blockSize);
+        for (int j = 0; j < ZBlockNum; j++)
+        {
+            for (int i = 0; i < XBlockNum; i++)
+            {
+                GameObject b = Instantiate(block, new Vector3(transform.position.x + blockSize * i, transform.position.y, transform.position.z + blockSize * j), Quaternion.identity, transform);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
