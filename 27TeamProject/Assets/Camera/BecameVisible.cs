fd2ae042c8a2e885b504e61ba7e465c81fbce52b
hanashiryuta <kbrn.kabirunrun@gmail.com>
2018-11-12 09:45:14 +0900

    Revert "Revert "カメラ関連追加""
    This reverts commit 8ec107f6319ce3b49d377dcc930d7a75bb58f7d7.

---
+++ 27TeamProject/Assets/Camera/BecameVisible.cs
@@ -0,0 +1,37 @@
+﻿///11月9日
+///石橋功基
+///左範囲チェック用クラス
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BecameVisible : MonoBehaviour {
+    // Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //チェック用のブロックが画面に表示されたとき呼び出す
+    void OnBecameVisible()
+    {
+        //カメラ取得
+        var cameraCtrl = Camera.main.GetComponent<CameraControl>();
+        cameraCtrl.CheckLeft();
+        cameraCtrl.CameraStop();
+    }
+
+    //チェック用ブロックが画面外になったとき呼び出す
+    void OnBecameInvisible()
+    {
+        //カメラ取得
+        var cameraCtrl = Camera.main.GetComponent<CameraControl>();
+        cameraCtrl.CheckLeft();
+    }
+}
---
+++ 27TeamProject/Assets/Camera/CameraControl.cs
@@ -0,0 +1,70 @@
+﻿///11月9日
+///石橋功基
+///カメラクラス
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public GameObject player;　//追従するプレイヤーを入れるオブジェクト
+    private Vector3 cameraPosition;　//カメラの初期値格納用
+    private bool checkLeft;　//カメラ範囲の左チェック用
+    private bool checkRight;　//カメラ範囲の右チェック用
+    private float x;　//プレイヤー追従のｘ座標
+    private Vector3 stopPosition;　//カメラ停止時のポジション格納用
+
+    // Use this for initialization
+    void Start()
+    {
+        //カメラ初期値格納
+        cameraPosition = transform.position;
+        checkLeft = false;
+        checkRight = false;
+    }	
+	// Update is called once per frame
+	void Update() {
+        //プレイヤー追従
+        CameraMove();
+    }
+
+    public void CheckLeft()
+    {
+        checkLeft = !checkLeft;
+    }
+
+    public void CheckRight()
+    {
+        checkRight = !checkRight;
+    }
+
+    public void CameraMove()
+    {
+        //左端にプレイヤーがいる場合
+        if (checkLeft && stopPosition.x > player.transform.position.x)
+        {
+            transform.position = transform.position;
+        }
+
+        //右端にプレイヤーがいる場合
+        else if (checkRight && stopPosition.x < player.transform.position.x)
+        {
+            transform.position = transform.position;
+        }
+
+        //プレイヤーの通常追従時
+        else
+        {
+            x = player.transform.position.x;
+            transform.position = new Vector3(x, cameraPosition.y, cameraPosition.z);
+        }
+    }
+
+    public void CameraStop()
+    {
+        //カメラ停止時ポジション格納
+        stopPosition = player.transform.position;
+    }
+}
