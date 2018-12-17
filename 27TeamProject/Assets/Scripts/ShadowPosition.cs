36240f080fd8830cf8dd924fd240526d0a24e0c4
hanashiryuta <kbrn.kabirunrun@gmail.com>
2018-12-17 11:37:09 +0900

    オブジェクトに影追加、文字の行間詰め追加
    オブジェクトにShadowを追加
    
    LetterSpacing.cs:新規作成
    
    ShadowPosition.cs:新規作成

---
+++ 27TeamProject/Assets/Scripts/ShadowPosition.cs
@@ -0,0 +1,21 @@
+﻿///
+///12/17
+///影クラス
+///葉梨竜太
+///
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowPosition : MonoBehaviour {
+    
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x, 1, transform.position.z);
+	}
+}
