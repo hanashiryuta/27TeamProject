293c778f2ff11d73c8ed697c206209646440dbc1
hanashiryuta <kbrn.kabirunrun@gmail.com>
2019-01-28 13:48:13 +0900

    Revert "Revert "BossLight完成・BossWarning完成""
    This reverts commit 77561842d03889f3200b94dff477b5b6623f7d2f.

--- 27TeamProject/Assets/Warning_Change.cs
+++ 27TeamProject/Assets/Warning_Change.cs
@@ -6,39 +6,52 @@ using UnityEngine.UI;
 public class Warning_Change : MonoBehaviour
 {
 
+   
     public Image im;
     [HideInInspector]
     public bool isUp = true;
-
+    int count;
     void Start()
     {
+        count = 0;
         im = im.GetComponent<Image>();
+        im.color = new Color(0, 0, 0, 0);
+
     }
 
     void Update()
     {
 
-        Color alpha = im.color;
-        alpha.a = 1.0f;
-        im.color = alpha;
-
-        if (im.color.b > 1)
+        if (count < 150)
         {
-            isUp = true;
+            if (im.color.b > 1)
+            {
+                isUp = true;
+            }
+            else if (im.color.b < 0)
+            {
+                isUp = false;
+            }
+            if (isUp == true)
+            {
+                //im.color -= Color.red / 1.0f * Time.deltaTime;
+                im.color -= Color.green / 1.0f * Time.deltaTime;
+                im.color -= Color.blue / 1.0f * Time.deltaTime;
+            }
+            else if (isUp == false)
+            {
+                im.color += Color.red / 1.0f * Time.deltaTime;
+                im.color += Color.green / 1.0f * Time.deltaTime;
+                im.color += Color.blue / 1.0f * Time.deltaTime;
+            }
+            count++;
+            Debug.Log(count);
         }
-        else if (im.color.b < 0)
+        else
         {
-            isUp = false;
-        }
-        if (isUp == true)
-        {
-            im.color -= Color.green / 1.0f * Time.deltaTime;
-            im.color -= Color.blue / 1.0f * Time.deltaTime;
-        }
-        else if (isUp == false)
-        {
-            im.color += Color.green / 1.0f * Time.deltaTime;
-            im.color += Color.blue / 1.0f * Time.deltaTime;
+            im.color -= Color.red / 1.5f * Time.deltaTime;
+            im.color -= Color.green / 1.5f * Time.deltaTime;
+            im.color -= Color.blue / 1.5f * Time.deltaTime;
         }
     }
 
